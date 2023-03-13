7e35d05d7353db28cc2c429ac02edd089be43f31
Hortense <utuje.hortense@epitech.eu>
2023-03-13 22:27:03 +0100

    [ADD]connected server

--- assets/js/rotationManager.js
+++ assets/js/rotationManager.js
@@ -2,9 +2,6 @@
 const players = ["Sara", "Hortense", "Celía", "Laia", "Alba", "Berta"]
 var pairs = [[players[0], players[1]], [players[2], players[3]], [players[4], players[5]]]
 
-console.log(players)
-console.log(pairs)
-
 var currentDate = thisweek();
 
 if (currentDate == nextDate) {
@@ -18,6 +15,7 @@ window.onload = function() {
 document.getElementById("thisweek").innerHTML=currentDate.toDateString();
 document.getElementById("nextweek").innerHTML=nextDate.toDateString();
 document.getElementById("nextnextweek").innerHTML=nextnextDate.toDateString();
+setTables();
 } 
 
 function nextweek(){
@@ -42,43 +40,56 @@ function thisweek() {
     return thisweek;
 }
 
-function timeManager() {
-    //check time now
-    
-    // Display the calculated result      
-    console.log("Week number of " + currentDate.toDateString())
-    console.log("Next week of " + nextDate.toDateString())
-    //set week now interval - check next week
-    //set date in table1
-    //set date in table2
-}
-
-function setTables() {
+async function setTables() {
     //rotate 3 times due to the number of tasks
+    var data = await getindexes();
+    iA = data.indexA,
+    iB = data.indexB,
+    iC = data.indexC,
+    document.getElementById("grpA1").innerHTML= pairs[iA[0]];
+    document.getElementById("grpA2").innerHTML= pairs[iA[1]];
+    document.getElementById("grpA3").innerHTML= pairs[iA[2]];
 
-    document.getElementById("grpA1").innerHTML= pairs[1];
-    document.getElementById("grpA2").innerHTML= pairs[2];
-    document.getElementById("grpA3").innerHTML= pairs[0];
+    document.getElementById("grpB1").innerHTML= pairs[iB[0]];
+    document.getElementById("grpB2").innerHTML= pairs[iB[1]];
+    document.getElementById("grpB3").innerHTML= pairs[iB[2]];
 
-    document.getElementById("grpB1").innerHTML= pairs[0];
-    document.getElementById("grpB2").innerHTML= pairs[1];
-    document.getElementById("grpB3").innerHTML= pairs[2];
+    document.getElementById("grpC1").innerHTML= pairs[iC[0]];
+    document.getElementById("grpC2").innerHTML= pairs[iC[1]];
+    document.getElementById("grpC3").innerHTML= pairs[iC[2]];
 
-    document.getElementById("grpC1").innerHTML= pairs[2];
-    document.getElementById("grpC2").innerHTML= pairs[0];
-    document.getElementById("grpC3").innerHTML= pairs[1];
-
-    // save in local storage
-
-    indexesA = [1,2,0];
-    indexesB = [0,1,2];
-    indexesC = [2,0,1];
 }
 
-function updateTables() {
-    indexesA = [1,2,0];
-    indexesB = [0,1,2];
-    indexesC = [2,0,1];
+async function getindexes() {
+    let obj;
+    const options = {method: 'GET', headers: {Accept: 'application/json'}};
+    const res = await fetch('http://127.0.0.1:8080/getindexes', options)
+  
+    obj = await res.json();
+  
+    console.log(obj)
+    return obj;
+  }
+
+  async function setindexes(payload) {
+    let obj;
+    const options = {method: 'POST', body: payload ,  headers: {Accept: 'application/json'}};
+    const res = await fetch('http://127.0.0.1:8080/setindexes', options)
+  
+    obj = await res.json();
+  
+    console.log(obj)
+    return obj;
+  }
+  
+
+async function updateTables() {
+    //connect to database
+    
+    var data = await getindexes();
+    indexesA = data.indexA,
+    indexesB = data.indexB,
+    indexesC = data.indexC,
 
     iA = calculateIndex(indexesA)
     iB = calculateIndex(indexesB)
@@ -87,11 +98,13 @@ function updateTables() {
     console.log("a1:", iA)
     console.log("b1:", iB)
     console.log("c1:", iC)
-
-    // update this week
-    // update next week
-
-    //update next next week
+    payload = {
+        indexA: iA,
+        indexB: iB,
+        indexC: iC
+    }
+    await setindexes(JSON.stringify(payload))
+    setTables();
 }
 
 function calculateIndex(indexes) {
--- server/myserver.js
+++ server/myserver.js
@@ -7,32 +7,13 @@
 'use strict';
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
-const open = require('open')
-const {MongoClient} = require('mongodb');
+//const open = require('open')
+const MongoClient = require('mongodb').MongoClient;
 
 const PORT = 8080;
 const HOST = 'localhost';
-
-//connect to db
-async function dbconnection() {
-    const uri = "mongodb+srv://mypiso:<i1tmMjT1HFDutT7o@pisoplannercluster0.bap6afz.mongodb.net/?retryWrites=true&w=majority";
-    
-    const client = new MongoClient(uri);
-    try {
-        await client.connect();
-    
-        await listDatabases(client);
-     
-    } catch (e) {
-        console.error(e);
-    }
-    finally {
-        await client.close();
-    }
-	// we'll add code here soon
-}
 //App
 const app = express()
 app.use(cors());
@@ -43,13 +24,38 @@ app.get('/', (req,res) => {
     res.send("Hello, server is running ...")
 });
 
-app.get('/getindexes', (req, res) => {
+app.get('/getindexes', async(req, res) => {
     //connect to dabase extract all indexes
-    dbconnection().catch(console.error);
+    
+    const uri = "mongodb+srv://mypiso:i1tmMjT1HFDutT7o@pisoplannercluster0.bap6afz.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    const db = client.db('PisoIndexes');
+    const collection = await db.collection("pairs");
+    const query = { name: "index" };
+    const doc = await collection.findOne(query);
+    res.send(doc)
 })
 
-app.post('/setindexes', (req, res) => {
+app.post('/setindexes', async (req, res) => {
     //update indexes in database
+    const uri = "mongodb+srv://mypiso:i1tmMjT1HFDutT7o@pisoplannercluster0.bap6afz.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    const db = client.db('PisoIndexes');
+    const collection = await db.collection("pairs");
+    // create a filter for a movie to update
+    const filter = { name: "index" };
+    // this option instructs the method to create a document if no documents match the filter
+    const options = { upsert: true };
+    // create a document that sets the plot of the movie
+    const updateDoc = {
+      $set: {
+        indexA: req.body.indexA,
+        indexB: req.body.indexB,
+        indexC: req.body.indexC,
+      },
+    };
+    const result = await collection.updateOne(filter, updateDoc, options);
+
 })
 //app.listen(PORT, HOST);
 app.listen(PORT, function () {
